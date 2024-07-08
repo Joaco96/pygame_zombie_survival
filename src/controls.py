cf7da8a87cd090a6de3b6a7b015f7e1c5b4b6e31
Joaco96 <joacocortes96@gmail.com>
2024-07-08 18:16:58 -0300

    final version no sprites v02

--- src/controls.py
+++ src/controls.py
@@ -21,6 +21,10 @@ def controls(SCREEN,FONT,settings):
 
         SCREEN.fill(settings["BLACK"])
         show_text(SCREEN,(settings["WIDTH"]//2,100),"Controls",FONT,settings["RED"])
+        show_text(SCREEN,(settings["WIDTH"]//2,180),"[A] [W] [S] [D] -> Movimiento personaje",FONT,settings["YELLOW"])
+        show_text(SCREEN,(settings["WIDTH"]//2,230),"Click IZQ Mouse -> Disparo",FONT,settings["YELLOW"])
+        show_text(SCREEN,(settings["WIDTH"]//2,280),"[P] -> Pausa",FONT,settings["YELLOW"])
+        show_text(SCREEN,(settings["WIDTH"]//2,330),"[M] -> Mute",FONT,settings["YELLOW"])
         show_text(SCREEN,(settings["WIDTH"]//2,500),"Press ENTER for Main Menu",FONT,settings["WHITE"])
         
         # actualizar pantalla
