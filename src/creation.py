d79ad1b832a1fb0f3a879e97772ae5dc4a8d6c37
Joaco96 <joacocortes96@gmail.com>
2024-07-07 22:13:38 -0300

    first preview no sprites

--- src/creation.py
+++ src/creation.py
@@ -8,14 +8,14 @@ def create_block(imagen:pygame.Surface = None,left=0,top=0,width=50,height=50,co
     return {"rect":pygame.Rect(left,top,width,height),"color":color,"borde":borde,"radio":radio, "img":imagen}
 
 def create_player(settings,imagen:pygame.Surface = None):
-    return create_block(imagen,settings["WIDTH"]//2-settings["PLAYER_WIDTH"],settings["HEIGHT"]//2-settings["PLAYER_HEIGHT"],settings["PLAYER_WIDTH"],settings["PLAYER_HEIGHT"],settings["WHITE"])
+    return create_block(imagen,settings["WIDTH"]//2-settings["PLAYER_WIDTH"],settings["HEIGHT"]//2-settings["PLAYER_HEIGHT"],settings["PLAYER_WIDTH"],settings["PLAYER_HEIGHT"],settings["WHITE"],radio=settings["PLAYER_HEIGHT"]//2)
 
 def create_zombie(settings,imagen:pygame.Surface = None):
     random_position = generate_random_position(settings)
-    return create_block(imagen,random_position[0],random_position[1],settings["ZOMBIE_WIDTH"],settings["ZOMBIE_HEIGHT"],settings["YELLOW"])
+    return create_block(imagen,random_position[0],random_position[1],settings["ZOMBIE_WIDTH"],settings["ZOMBIE_HEIGHT"],settings["YELLOW"],radio=settings["ZOMBIE_HEIGHT"]//2)
 
 def create_bullet(settings,origen:tuple[int,int], target:tuple[int,int],imagen:pygame.Surface = None):
-    bullet = create_block(imagen,origen[0]-settings["BULLET_WIDTH"]//2,origen[1]-settings["BULLET_HEIGHT"]//2, settings["BULLET_WIDTH"], settings["BULLET_HEIGHT"],settings["RED"])
+    bullet = create_block(imagen,origen[0]-settings["BULLET_WIDTH"]//2,origen[1]-settings["BULLET_HEIGHT"]//2, settings["BULLET_WIDTH"], settings["BULLET_HEIGHT"],settings["RED"],radio=settings["BULLET_HEIGHT"]//2)
     direccion = mov_direccion(origen,target,settings["BULLET_SPEED"])
     bullet["direc"] = direccion  
     return bullet
